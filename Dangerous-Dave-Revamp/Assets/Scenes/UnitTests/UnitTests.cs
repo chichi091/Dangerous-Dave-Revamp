c2fb393b428de50f28f258781919679c99088144
chichi091 <u20632429@tuks.co.za>
2024-05-23 11:47:25 +0200

    :white_check_mark: :test_tube: Add passing and failing tests

---
+++ Dangerous-Dave-Revamp/Assets/Scenes/UnitTests/UnitTests.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.SceneManagement;
+
+public class UnitTests
+{
+    // A Test behaves as an ordinary method
+    [Test]
+    public void UnitTestsSimplePasses()
+    {
+        
+    }
+
+    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+    // `yield return null;` to skip a frame.
+    [UnityTest]
+    public IEnumerator UnitTestsWithEnumeratorPasses()
+    {
+        // Use the Assert class to test conditions.
+        // Use yield to skip a frame.
+        yield return null;
+    }
+}
